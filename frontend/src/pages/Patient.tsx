30db223e3f4491d05cec7b813336bc4faacfa0f0
chirag-ghosh <cghosh828049@gmail.com>
2023-03-02 20:37:06 +0530

    Add patient search bar;

--- frontend/src/pages/Patient.tsx
+++ frontend/src/pages/Patient.tsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
+import { BiSearch } from 'react-icons/bi'
+
 function Patient() {
-    return <div className="home">This website is a work in progress.</div>;
-  }
+
+  const [query, setQuery] = useState<string>("")
+
+  return (
+    <div className="patient">
+      <div className="search">
+        <BiSearch size={20} className="search-icon" />
+        <input
+          id="patient-query"
+          type='search'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Type here to search patients..."
+        />
+      </div>
+    </div>
+  );
+}
   
   export default Patient;
   
