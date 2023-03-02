08c475690c6f620f17c1c7f5b54583b0cc155b24
chirag-ghosh <cghosh828049@gmail.com>
2023-03-02 15:18:21 +0530

    Update header title;

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,24 @@
 import { useAuthContext } from "../hooks/useAuthContext";
 import { IoIosMedkit } from 'react-icons/io'
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 function Header() {
 
     const {user} = useAuthContext()
+    const location = useLocation()
+
+    const [title, setTitle] = useState<string>('Home')
+
+    useEffect(() => {
+        setTitle(location.pathname.split("/")[1].toUpperCase() || "HOME")
+    }, [location.pathname])
 
     if(!user) return null;
 
     return(
         <div className="header">
-            <div className="title">Dashboard</div>
+            <div className="title">{title}</div>
             <div className="profile">
                 <div className="profile-img-wrapper">
                     <IoIosMedkit size={25} />
