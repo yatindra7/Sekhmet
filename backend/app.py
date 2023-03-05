21856db66a819323c55fc991af07795dd65c66c7
Yatindra Indoria <84220034+yatindra7@users.noreply.github.com>
2023-03-06 00:50:08 +0530

    all tested CTRL-F @Chirag to see the forms

---
+++ backend/app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+# configs (to be changed)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sekhmet.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+# the routes
+from routes import *
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    from .models import *
+    db.create_all()
+
+    patient_data =[
+        Patient(SSN= 100000001,Name ='John Smith',Address = '42 Foobar Lane',Phone = '555-0256',InsuranceID= 68476213,PCP=1, Gender = 'Male', Age =52),
+        Patient(SSN= 100000002,Name ='Grace Ritchie',Address = '37 Snafu Drive',Phone = '555-0512',InsuranceID= 36546321,PCP=2, Gender = 'Male', Age =52),
+        Patient(SSN= 100000003,Name ='Random J. Patient',Address = '101 Omgbbq Street',Phone = '555-1204',InsuranceID= 65465421,PCP=2, Gender = 'Male', Age =52),
+        Patient(SSN= 100000004,Name ='Dennis Doe',Address = '1100 Foobaz Avenue',Phone = '555-2048',InsuranceID= 68421879,PCP=3, Gender = 'Male', Age =52)
+    ]
+
+    db.session.add_all(patient_data)
+
+    print(type({patient_data[0].SSN:{
+        'Name': patient_data[0].Name
+        , 'Address': patient_data[0].Address
+        , 'Phone': patient_data[0].Phone
+        }}))
---
+++ backend/routes.py
@@ -0,0 +1,245 @@
+from app import app, db
+from models import User, Physician, Patient, Undergoes, Appointment
+from flask import request, make_response, jsonify
+
+from uuid import uuid4
+
+def sqlalchemy_row_to_dict(row):
+
+    return { column.name:str( getattr(row, column.name) ) for column in row.__table__.columns }
+
+UUID_RANGE_LIMIT = 1000000
+
+@app.route('/user', methods=['POST', 'GET'])
+def user():
+    
+    if request.method == 'POST':
+
+        # getting the response from the form
+        # @Chirag add these to the form
+
+        email = request.form.get('email')
+        role = request.form.get('role')
+        password = request.form.get('password')
+        name = request.form.get('name')
+        position = request.form.get('position')
+        ssn = request.form.get('ssn')
+
+        # adding the new user
+
+        unique_id = int(uuid4()) % UUID_RANGE_LIMIT
+        user = User(
+            id = unique_id
+            , email = email
+            , password = password
+            , role = role
+            )
+        
+        # inserting user into db
+        db.session.add(user)
+        db.session.commit()
+
+        # if doctor then also to be added
+        # into the Physician table
+
+        if role == 'Doctor':
+
+            physician = Physician(
+                EmployeeID = unique_id
+                , Name = name
+                , Position = position
+                , SSN = ssn
+            )
+
+            db.session.add(physician)
+            db.session.commit()
+
+        return make_response(jsonify(
+                {
+                    "message": "User created"
+                }
+            ) , 201)
+    
+    # get all users in db
+
+    return make_response(jsonify(
+            {
+                "message": "Retreived all patient"
+                , "users": [sqlalchemy_row_to_dict(user) for user in User.query.all()]
+            }
+        ), 200)
+
+@app.route('/user/login', methods=['POST', 'GET'])
+def user_login():
+
+    # todo
+
+    if request.method == 'POST':
+        return 'USER LOGIN POST'
+    return 'USER LOGIN GET'
+
+@app.route('/patient', methods=['POST', 'GET'])
+def patient():
+
+    if request.method == 'POST':
+        
+        # @Chirag the forms
+        ssn = request.form.get('ssn')
+        name = request.form.get('name')
+        address = request.form.get('address')
+        phone = request.form.get('phone')
+        insurance_id = request.form.get('insurance_id')
+        pcp = request.form.get('pcp')
+        gender = request.form.get('gender')
+        age = request.form.get('age')
+
+        patient = Patient(
+
+            SSN = ssn
+            , Name = name
+            , Address = address
+            , Phone = phone
+            , InsuranceID = insurance_id
+            , PCP = pcp
+            , Gender = gender
+            , Age = age
+        )
+
+        db.session.add(patient)
+        db.session.commit()
+        
+        return make_response(jsonify(
+                {
+                    "message": "Patient created"
+                }
+            ) , 201)
+    
+    # get all patients in db
+
+    return make_response(jsonify(
+            {
+                "message": "Retreived all patients"
+                , "patients": [sqlalchemy_row_to_dict(patient) for patient in Patient.query.all()]
+            }
+        ), 200)
+
+@app.route('/patient/<int:ssn>')
+def patient_ssn(ssn):
+
+    # getting all the patient information
+
+    # getting the patient basic data
+    patient = Patient.query.filter_by(SSN = ssn).first()
+    if patient:
+        patient = sqlalchemy_row_to_dict(patient)
+    else:
+        return make_response(
+            jsonify(
+                {
+                    "message": "Not found"
+                }
+            ), 404
+        )
+
+    # undergoes data
+    undergoes = Undergoes.query.filter_by(Patient = ssn)
+    if undergoes:
+        undergoes = [sqlalchemy_row_to_dict(undergone) for undergone in undergoes]
+    # appointments
+    appointments =  Appointment.query.filter_by(Patient = ssn)
+    if appointments:
+        appointments = [sqlalchemy_row_to_dict(appointment) for appointment in appointments]
+
+    # appointment data
+    return make_response(
+        jsonify(
+            {
+                "patient": patient
+                , "undergoes": undergoes
+                , "appointments": appointments
+            }
+        ), 200
+    )
+
+# @Shreya and @Chirag
+
+@app.route('/patient/<int:ssn>/appointment', methods=['POST', 'GET'])
+def patient_ssn_appointment(ssn):
+
+    if request.method == 'POST':
+        return f'PATIENT {ssn} APPOINTMENT POST'
+    return f'PATIENT {ssn} APPOINTMENT GET'
+
+# @Shreya and @Chirag
+
+@app.route('/patient/<int:ssn>/test', methods=['POST', 'GET'])
+def patient_ssn_test(ssn):
+
+    if request.method == 'POST':
+        return f'PATIENT {ssn} TEST POST'
+    return f'PATIENT {ssn} TEST GET'
+
+@app.route('/physician', methods=['POST', 'GET'])
+def physician():
+
+    if request.method == 'POST':
+
+        # extract the physician info recvd
+
+        # add to db
+
+        return 'PHYSICIAN POST'
+    
+    # get all physicians in DB
+    return 'PHYSICIAN GET'
+
+@app.route('/physician/<int:id>')
+def physician_id(id):
+
+    # get physician info (all)
+    
+    # get the patient info from appointment
+    
+
+    return f'PHYSCIAN {id} GET'
+
+@app.route('/physician/engagements')
+def physician_engagements():
+
+    # get physician schedule (basically all appointments)
+    return 'PHYSCIAN ENGAGEMENT GET'
+
+@app.route('/procedure')
+def procedure():
+
+    # get all procedures
+    return 'PROCEDURE GET'
+
+@app.route('/procedure/<int:id>', methods=['PATCH'])
+def procedure_id(id):
+
+    if request.method == 'PATCH':
+
+        # adding the recvd file to Undergoes
+        return f'PROCEDURE {id} PATCH'
+    return f'PROCEDURE {id} GET'
+
+@app.route('/medication')
+def medication():
+
+    # get all medications
+    return 'MEDICATION GET'
+
+@app.route('/appointment/<int:id>', methods=['PATCH', 'GET'])
+def appointment_id(id):
+
+    if request.method == 'PATCH':
+
+        return f'APPOINTMENT {id} PATCH'
+    return f'APPOINTMENT {id} GET'
+
+@app.route('/notify')
+def notify():
+
+    # send mails
+    return 'NOTIFY MAILS'
