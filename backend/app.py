612b07bf07e223087b0157d4b711f30f708ac6f2
chirag-ghosh <cghosh828049@gmail.com>
2023-03-09 18:23:41 +0530

    Update backend setup;

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ from flask_jwt_extended import JWTManager
 
 from dotenv import load_dotenv
 import sys
+import os
 
 load_dotenv()
 
@@ -14,24 +15,24 @@ if len(sys.argv) != 2:
     print("Usage: app.py <db_name>.db")
 
 # give as first argument
-dbname = sys.argv[1]
-_uname = 'yatindra'
-_password = 'Ayhind2123'
-_host = 'localhost'
+dbname = os.getenv('DBNAME')
+_uname = os.getenv('UNAME')
+_password = os.getenv('PASSWORD')
+_host = os.getenv('HOST')
 
 app = Flask(__name__)
 
 # configs (to be changed)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{_uname}:{_password}@{_host}/{dbname}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_ERROR_MESSAGE_KEY'] = 'message'
 
-CORS(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 login_manager = LoginManager(app)
+CORS(app)
 
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
