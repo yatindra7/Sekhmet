a957c32b9402ee81545d0fb9dba852e49b4f4efb
chirag-ghosh <cghosh828049@gmail.com>
2023-03-09 23:35:34 +0530

    Update image link;

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,27 +1,36 @@
-import { NavLink, useNavigate } from "react-router-dom";
-import { BiHomeAlt } from 'react-icons/bi'
-import { RiAdminLine, RiHotelBedFill, RiLogoutBoxRLine, RiStethoscopeLine } from 'react-icons/ri'
-import { useAuthContext } from "../hooks/useAuthContext";
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/click-events-have-key-events */
+
+import { NavLink, useNavigate } from 'react-router-dom';
+import { BiHomeAlt } from 'react-icons/bi';
+import { RiAdminLine, RiHotelBedFill, RiLogoutBoxRLine, RiStethoscopeLine } from 'react-icons/ri';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 function Sidebar() {
+  const navigate = useNavigate();
+  const { logoutHandler } = useAuthContext();
 
-    let navigate = useNavigate();
-    let { logoutHandler } = useAuthContext();
-
-    return(
-        <div className="sidebar">
-            <img src="/src/assets/logo.png" alt="sekhmet" onClick={() => navigate("/")} />
-            <div className="link-list">
-                <NavLink to="/"><BiHomeAlt size={30} /></NavLink>
-                <NavLink to="/doctor"><RiStethoscopeLine size={30} /></NavLink>
-                <NavLink to="/patient"><RiHotelBedFill size={30} /></NavLink>
-                <NavLink to="/admin"><RiAdminLine size={30} /></NavLink>
-            </div>
-            <div className="logout">
-                <RiLogoutBoxRLine size={30} onClick={() => logoutHandler()} />
-            </div>
-        </div>
-    )
+  return (
+    <div className="sidebar">
+      <img src="/logo.png" alt="sekhmet" onClick={() => navigate('/')} />
+      <div className="link-list">
+        <NavLink to="/">
+          <BiHomeAlt size={30} />
+        </NavLink>
+        <NavLink to="/doctor">
+          <RiStethoscopeLine size={30} />
+        </NavLink>
+        <NavLink to="/patient">
+          <RiHotelBedFill size={30} />
+        </NavLink>
+        <NavLink to="/admin">
+          <RiAdminLine size={30} />
+        </NavLink>
+      </div>
+      <div className="logout">
+        <RiLogoutBoxRLine size={30} onClick={() => logoutHandler()} />
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -54,7 +54,7 @@ function Login() {
     <div className="login">
       <div className="form-wrapper">
         <div className="top">
-          <img src="/src/assets/logo.png" alt="sekhmet" />
+          <img src="/logo.png" alt="sekhmet" />
           <div className="title">Sekhmet</div>
         </div>
         <div className="subtitle">Hi! Please enter your credentials to get access to the hospital of heavens.</div>
